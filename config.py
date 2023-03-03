c9d1c25c14302e93cdf9a74c5f4f542c8c550e1c
AkXbots <122038135+AkXbots@users.noreply.github.com>
2023-03-03 13:50:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_ID = int(getenv("API_ID", "6435225")) #optional
 API_HASH = getenv("API_HASH", "") #optional
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = int(getenv("OWNER_ID"," 5940604852"))
+OWNER_ID = int(getenv("OWNER_ID"))
 MONGO_URL = getenv("MONGO_URL")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 ALIVE_PIC = getenv("ALIVE_PIC", 'https://te.legra.ph/file/2ac1c953d54544cc79dda.jpg')
@@ -21,7 +21,7 @@ GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/ITZ-ZAID/ZAID-USERBOT")
 BRANCH = getenv("BRANCH", "master") #don't change
  
-STRING_SESSION1 = getenv("STRING_SESSION1", "BQBiMZkADTK1DD1FYSGjOCImJjT6MrB7XDjcpimZU2-QGnqrltifkXAwPGm8K0-y3aTLp-UxEa08JifPNhDQQlkUdXFhMNch_N1DNgudOco1uJZZkbBxMU3ZwWN2-QrTncat5lLa77nisH4s0qdqTJwzJgB9VNHP0dZtWmsUutMRfQIk97kmhkL09eW0mvuD9u6Fr0NibIlTiZoBJKbuKXilYnlGGsfJETyFAeCuZaaLJPahNLRaQAzKXR-uoN77d0HjycIvtmYw3L4qeOunZxEWfwyfV_p-9q_NT21uiMqAlgnW9P5FDq8zvfDojXwdHnyv3Ql9H4nGbRViF_7kRpmr_TMwNQAAAAF2DKVvAA")
+STRING_SESSION1 = getenv("STRING_SESSION1", "")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
